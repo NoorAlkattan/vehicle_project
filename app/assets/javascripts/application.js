dad50ae228e89626494e10307e3517f2f89e6af3
Noor <noor_samir@yahoo.com>
2016-10-20 23:57:28 +0000

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,30 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+<script>
+       function validateForm() {
+         var x = document.forms["myForm"]["firs_name"].value;
+         if (x == null || x == "") {
+         alert("Enter your first_name");
+         return false;
+         }
+       
+         var y = document.forms["myForm"]["last_name"].value;
+         if (y == null || y == "") {
+         alert("Enter your Last_name");
+         return false;
+         }
+         var s = document.forms["myForm"]["email"].value;
+         var atpos = s.indexOf("@");
+         var dotpos = s.lastIndexOf(".");
+         if (atpos<1 || dotpos<atpos+2 || dotpos+2>=s.length) {
+         alert("Enter a valid email");
+     return false;
+       }
+        
+       var z = document.forms["myForm"]["phonenumber"].value; 
+       if (z == null || z == "") {
+       alert("Enter your Phone Number");
+       return false;
+}  
+}  
